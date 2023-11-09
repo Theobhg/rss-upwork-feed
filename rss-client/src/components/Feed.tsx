813210fb6562ef3f4167132e79590121a23e3cff
Theo Bhg <theobhg@Theos-MacBook-Pro.local>
2023-11-09 11:02:52 -0300

    Add and configure socketio-client & add some styles

--- rss-client/src/components/Feed.tsx
+++ rss-client/src/components/Feed.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect, FC } from 'react';
 import axios from 'axios';
 import htmlReactParse from 'html-react-parser';
+import { io } from 'socket.io-client';
+
 import { RSSFeedItem } from '../interfaces/rss-feed-item';
 
 const Feed: FC = () => {
   const [articles, setArticles] = useState<RSSFeedItem[]>([]);
 
-  console.log(articles);
   const getArticles: () => Promise<void> = async () => {
     try {
       const response = await axios.get('http://localhost:3333');
@@ -18,18 +19,26 @@ const Feed: FC = () => {
   };
 
   useEffect(() => {
+    const socket = io('http://localhost:3333');
     getArticles();
+
+    socket.on('feed-update', (updatedItems: RSSFeedItem[]) => {
+      setArticles(updatedItems);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   return (
     <div>
-      <ul>
+      <ul className="p-16">
         {articles.map(({ title, link, content, guid }) => (
-          <li key={guid}>
-            <div className="md:container md:mx-auto mb-4 ">
-              <h1 className="text-xl font-bold">{title}</h1>
+          <li key={guid} className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
+            <div className="md:container md:mx-auto mb-4">
+              <h1 className="text-2xl font-bold mb-2">{title}</h1>
               <div>{htmlReactParse(content)}</div>
-
               <div>
                 <a href={link} target="_blank">
                   link
