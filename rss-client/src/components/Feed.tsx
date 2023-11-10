8f7e374a67076c4706ca1ad0a92c44ee31835767
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-10 01:43:02 +0000

    Added initial page loading as LoadSpinner.tsx to Feed.tsx

--- rss-client/src/components/Feed.tsx
+++ rss-client/src/components/Feed.tsx
@@ -4,52 +4,61 @@ import htmlReactParse from 'html-react-parser';
 import { io } from 'socket.io-client';
 
 import { RSSFeedItem } from '../interfaces/rss-feed-item';
+import LoadSpinner from './LoadSpinner';
 
 const Feed: FC = () => {
-  const [articles, setArticles] = useState<RSSFeedItem[]>([]);
+   const [articles, setArticles] = useState<RSSFeedItem[]>([]);
+   const [isLoading, setIsLoading] = useState<Boolean>(true);
 
-  const getArticles: () => Promise<void> = async () => {
-    try {
-      const response = await axios.get('http://localhost:3333');
+   const getArticles: () => Promise<void> = async () => {
+      try {
+         const response = await axios.get('http://localhost:3333');
+         setArticles(response.data);
+      } catch (error) {
+         console.log(error);
+      }
+   };
 
-      setArticles(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+   useEffect(() => {
+      const socket = io('http://localhost:3333');
+      getArticles();
 
-  useEffect(() => {
-    const socket = io('http://localhost:3333');
-    getArticles();
+      socket.on('feed-update', (updatedItems: RSSFeedItem[]) => {
+         setArticles(updatedItems);
 
-    socket.on('feed-update', (updatedItems: RSSFeedItem[]) => {
-      setArticles(updatedItems);
-    });
+         if (isLoading) {
+            setIsLoading(false);
+         }
+      });
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+      return () => {
+         socket.disconnect();
+      };
+   }, []);
 
-  return (
-    <div>
-      <ul className="p-16">
-        {articles.map(({ title, link, content, guid }) => (
-          <li key={guid} className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
-            <div className="md:container md:mx-auto mb-4">
-              <h1 className="text-2xl font-bold mb-2">{title}</h1>
-              <div>{htmlReactParse(content)}</div>
-              <div>
-                <a href={link} target="_blank">
-                  link
-                </a>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+   return (
+      <div>
+         {isLoading ? (
+            <LoadSpinner />
+         ) : (
+            <ul className="p-16">
+               {articles.map(({ title, link, content, guid }) => (
+                  <li key={guid} className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
+                     <div className="md:container md:mx-auto mb-4">
+                        <h1 className="text-2xl font-bold mb-2">{title}</h1>
+                        <div>{htmlReactParse(content)}</div>
+                        <div>
+                           <a href={link} target="_blank">
+                              link
+                           </a>
+                        </div>
+                     </div>
+                  </li>
+               ))}
+            </ul>
+         )}
+      </div>
+   );
 };
 
 export default Feed;
