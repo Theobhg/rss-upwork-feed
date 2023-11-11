7b4ed75c5099e1e73c7535dbbd5516dc9b73bdc8
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-11 00:28:44 +0000

    Used import.meta.env to access .env variables and replaced default server url endpoint string literals ('http://localhost:3333')

--- rss-client/src/components/Feed.tsx
+++ rss-client/src/components/Feed.tsx
@@ -2,54 +2,56 @@ import { useState, useEffect, FC } from 'react';
 import axios from 'axios';
 import htmlReactParse from 'html-react-parser';
 import { io } from 'socket.io-client';
-
 import { RSSFeedItem } from '../interfaces/rss-feed-item';
 
+// Accessing environment variables from the.env file with Vite's import.meta.env;
+const viteMetaEnv = import.meta.env;
+
 const Feed: FC = () => {
-  const [articles, setArticles] = useState<RSSFeedItem[]>([]);
+   const [articles, setArticles] = useState<RSSFeedItem[]>([]);
 
-  const getArticles: () => Promise<void> = async () => {
-    try {
-      const response = await axios.get('http://localhost:3333');
+   const getArticles: () => Promise<void> = async () => {
+      try {
+         const response = await axios.get(viteMetaEnv.VITE_SERVER_URL);
 
-      setArticles(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+         setArticles(response.data);
+      } catch (error) {
+         console.log(error);
+      }
+   };
 
-  useEffect(() => {
-    const socket = io('http://localhost:3333');
-    getArticles();
+   useEffect(() => {
+      const socket = io(viteMetaEnv.VITE_SERVER_URL);
+      getArticles();
 
-    socket.on('feed-update', (updatedItems: RSSFeedItem[]) => {
-      setArticles(updatedItems);
-    });
+      socket.on('feed-update', (updatedItems: RSSFeedItem[]) => {
+         setArticles(updatedItems);
+      });
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+      return () => {
+         socket.disconnect();
+      };
+   }, []);
 
-  return (
-    <div>
-      <ul className="p-16">
-        {articles.map(({ title, link, content, guid }) => (
-          <li key={guid} className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
-            <div className="md:container md:mx-auto mb-4">
-              <h1 className="text-2xl font-bold mb-2">{title}</h1>
-              <div>{htmlReactParse(content)}</div>
-              <div>
-                <a href={link} target="_blank">
-                  link
-                </a>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+   return (
+      <div>
+         <ul className="p-16">
+            {articles.map(({ title, link, content, guid }) => (
+               <li key={guid} className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
+                  <div className="md:container md:mx-auto mb-4">
+                     <h1 className="text-2xl font-bold mb-2">{title}</h1>
+                     <div>{htmlReactParse(content)}</div>
+                     <div>
+                        <a href={link} target="_blank">
+                           link
+                        </a>
+                     </div>
+                  </div>
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
 };
 
 export default Feed;
