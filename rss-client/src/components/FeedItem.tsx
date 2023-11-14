2a8074755bbdbdfafcb397efc7aca43d65fe93d3
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-14 17:41:21 +0000

    FIXED [unreported]: Parsed article's title whit HTMLReactParser

--- rss-client/src/components/FeedItem.tsx
+++ rss-client/src/components/FeedItem.tsx
@@ -7,15 +7,17 @@ const FeedItem = ({ article }: { article: RSSFeedItem }) => {
    const { title, link, content } = article;
 
    // Spliting the title into two parts to apply separate styling (green Upwork and black title)
-   const upworkTitle = sliceSubstring(title, -'Upwork'.length);
    const normalTitle = sliceSubstring(title, 0, -'Upwork'.length);
+   const upworkTitle = sliceSubstring(title, -'Upwork'.length);
 
    return (
       <li className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
          <div className="md:container md:mx-auto mb-4">
             <h1 className="text-2xl font-bold mb-2">
-               {normalTitle}
-               <span className="text-2xl font-bold mb-2 text-primary">{upworkTitle}</span>
+               {HTMLReactParser(normalTitle)}
+               <span className="text-2xl font-bold mb-2 text-primary">
+                  {HTMLReactParser(upworkTitle)}
+               </span>
             </h1>
             <div>{HTMLReactParser(content)}</div>
             <div>
