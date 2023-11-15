83f0afa91eaf935be82c684233b139cb61888362
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-15 06:30:51 +0000

    Created blue new tag, added isNew prop to rss feed item interfaces, criated custom feed update control function in src/utils to check for actual feed updates (new items)

--- rss-server/src/interfaces/rss-feed.ts
+++ rss-server/src/interfaces/rss-feed.ts
@@ -1,36 +1,37 @@
 export interface RSSFeed {
-  description: string;
-  feedUrl: string;
-  generator: string;
-  image: RSSFeedImage;
-  items: RSSFeedItem[];
-  lastBuildDate: string;
-  link: string;
-  paginationLinks: RSSFeedPaginationLinks;
-  title: string;
-  webMaster: string;
+   description: string;
+   feedUrl: string;
+   generator: string;
+   image: RSSFeedImage;
+   items: RSSFeedItem[];
+   lastBuildDate: string;
+   link: string;
+   paginationLinks: RSSFeedPaginationLinks;
+   title: string;
+   webMaster: string;
 }
 
 export interface RSSFeedItem {
-  creator: string;
-  title: string;
-  link: string;
-  pubDate: string;
-  content: string;
-  guid: string;
-  categories: string[];
-  isoDate: string;
-  contentEncodedSnippet: string;
-  dcCreator: string;
-  contentSnippet: string;
+   isNew: boolean;
+   creator: string;
+   title: string;
+   link: string;
+   pubDate: string;
+   content: string;
+   guid: string;
+   categories: string[];
+   isoDate: string;
+   contentEncodedSnippet: string;
+   dcCreator: string;
+   contentSnippet: string;
 }
 
 interface RSSFeedImage {
-  link: string;
-  url: string;
-  title: string;
+   link: string;
+   url: string;
+   title: string;
 }
 
 interface RSSFeedPaginationLinks {
-  self: string;
+   self: string;
 }
---
+++ rss-server/src/utils/feedControl.ts
@@ -0,0 +1,34 @@
+import { RSSFeedItem } from '../interfaces/rss-feed';
+
+const getFeedItemsGuidsSet = (feedItems: RSSFeedItem[]) => {
+   return new Set<string>(feedItems.map(feedItem => feedItem.guid));
+};
+
+const isFeedItemsChanged = (
+   updatedFeedItems: RSSFeedItem[],
+   previousFeedItemsGuidsSet: Set<string>
+) => {
+   return updatedFeedItems.some(updtFeedItem => !previousFeedItemsGuidsSet.has(updtFeedItem.guid));
+};
+
+const newFeedItems = (updatedFeedItems: RSSFeedItem[], previousFeedItems: RSSFeedItem[]) => {
+   const previousFeedItemsGuidsSet = getFeedItemsGuidsSet(previousFeedItems);
+
+   if (isFeedItemsChanged(updatedFeedItems, previousFeedItemsGuidsSet)) {
+      updatedFeedItems.forEach(updtFeedItem => {
+         if (!previousFeedItemsGuidsSet.has(updtFeedItem.guid)) {
+            updtFeedItem.isNew = true;
+         } else {
+            updtFeedItem.isNew = false;
+         }
+      });
+
+      previousFeedItems.length = 0;
+      updatedFeedItems.forEach(updtFeedItem => {
+         previousFeedItems.push(updtFeedItem);
+      });
+   }
+   return previousFeedItems;
+};
+
+export { getFeedItemsGuidsSet, isFeedItemsChanged, newFeedItems };
