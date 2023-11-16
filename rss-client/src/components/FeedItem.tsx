242f0d96839790d2eabc479db8519b5130bff437
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-16 17:54:48 +0000

    Fixed [unreported]: Refactored string splitting method

--- rss-client/src/components/FeedItem.tsx
+++ rss-client/src/components/FeedItem.tsx
@@ -1,22 +1,21 @@
 import HTMLReactParser from 'html-react-parser';
 
 import { RSSFeedItem } from '../interfaces/rss-feed-item';
-import sliceSubstring from '../utils/sliceSubstring';
+import splitStringInHalf from '../utils/splitStringInHalf';
 
 const FeedItem = ({ article }: { article: RSSFeedItem }) => {
    const { title, link, content } = article;
 
    // Spliting the title into two parts to apply separate styling (green Upwork and black title)
-   const normalTitle = sliceSubstring(title, 0, -'Upwork'.length);
-   const upworkTitle = sliceSubstring(title, -'Upwork'.length);
+   const splitTitle = splitStringInHalf(title, 'Upwork');
 
    return (
       <li className="border border-gray-300 py-8 mb-8 rounded-lg shadow-sm">
          <div className="md:container md:mx-auto mb-4">
             <h1 className="text-2xl font-bold mb-2">
-               {HTMLReactParser(normalTitle)}
+               {HTMLReactParser(splitTitle.firstHalf)}
                <span className="text-2xl font-bold mb-2 text-primary">
-                  {HTMLReactParser(upworkTitle)}
+                  {HTMLReactParser(splitTitle.secondHalf)}
                </span>
             </h1>
             <div>{HTMLReactParser(content)}</div>
--- rss-client/src/utils/sliceSubstring.ts
+++
@@ -1,5 +0,0 @@
-const sliceSubstring = (string: string, startIndex?: number, endIndex?: number): string => {
-   return string.slice(startIndex, endIndex);
-};
-
-export default sliceSubstring;
